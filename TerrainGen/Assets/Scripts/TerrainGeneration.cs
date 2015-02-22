9015f16f764daea79c37e10403b253dc6893cb8a
matortheeternal <why101@msn.com>
2015-02-21 19:22:38 -0800

    adjustments
    various adjustments to correct game logic.
    moved terrain generation script to an empty game object.

--- TerrainGen/Assets/Scripts/TerrainGeneration.cs
+++ TerrainGen/Assets/Scripts/TerrainGeneration.cs
@@ -6,11 +6,12 @@ using System.Collections.Generic;
 public class TerrainGeneration : MonoBehaviour {
   
   public Sprite sprite1;
-  public int worldSize = 64;
-  public float spriteSize = 2.52f;
+  public int worldSize = 128;
+  public float spriteSize = 0.4f;
   
 	// generate terrain
 	void Start () {
+    // loop to create sprites at world coordinates
     int startPos = 0 - worldSize/2;
     int endPos = worldSize/2 - 1;
     for (int i = startPos; i < endPos; i++) {
@@ -22,14 +23,17 @@ public class TerrainGeneration : MonoBehaviour {
   
   // used to create a new sprite game object at the specified coordinates
   void CreateSprite (int x, int y) {
+    // create and instantiate new Game Object to hold sprite
     string name = "Terrain(" + x.ToString() + "," + y.ToString() + ")";
     GameObject go = new GameObject(name);
-    Instantiate(go, transform.position, Quaternion.identity);
+    
+    // create sprite renderer on game object, and set to render our sprite
     SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
     renderer.sprite = sprite1;
-    Transform t = go.GetComponent<Transform>();
+    
+    // move sprite to correct x,y location
     float xpos = x * spriteSize;
     float ypos = y * spriteSize;
-    t.position = new Vector3(xpos, ypos, 0);
+    go.transform.position = new Vector3(xpos, ypos, 0);
   }
 }
